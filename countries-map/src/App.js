a03225c1510ac8fb42545307c24d243e2ed23aae
leo tanguy <leo0703@hotmail.fr>
2022-04-04 15:07:33 +0200

    Finished Countries List

---
+++ countries-map/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./pages/About";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Le path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ countries-map/src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt velit inventore veritatis magnam iste iusto error veniam recusandae. Blanditiis expedita illum accusamus facere, eaque minima. Iste, voluptas ad beatae debitis quisquam aspernatur? Facilis laboriosam omnis voluptatibus ipsa, minus cum eaque nemo distinctio minima. Asperiores, error! Cum quo necessitatibus beatae, eius maiores exercitationem soluta consectetur iste facere unde consequuntur, minus, enim ullam repudiandae est possimus nobis? Omnis veritatis natus tempora temporibus eos ut deleniti! Quas quod pariatur corrupti eos fugiat, voluptatum sint architecto. Architecto quos molestiae maxime, veniam maiores vero accusantium? Reprehenderit, tenetur assumenda id saepe sint voluptas ducimus? Perspiciatis, sapiente.
+      </p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nisi voluptates tempore distinctio! Nulla reiciendis quo, labore tenetur eligendi architecto maxime? Iste, dignissimos placeat doloremque distinctio est earum repudiandae. Quod quidem aut, provident id et excepturi quis laudantium vero autem temporibus numquam nostrum laborum, consequuntur necessitatibus tempora minima eos quas alias dolor reiciendis libero adipisci commodi, ipsam dolores. Error neque recusandae, facere nam, hic nemo quia porro molestiae tempore rerum magni expedita commodi deserunt adipisci accusantium reprehenderit soluta itaque quae. Eaque fugiat magni expedita quaerat! Aspernatur dolorum eaque corporis excepturi, illum recusandae qui facilis laborum nihil iusto suscipit beatae voluptatum eveniet consequatur. Officiis quam autem sint ex veritatis debitis ipsum cumque architecto nihil dolorum. Culpa, sed. Ab aperiam totam excepturi obcaecati, numquam nulla eum aliquam libero quasi dolorem est asperiores accusamus quidem. Vitae minus quo, est nisi cupiditate maiores necessitatibus dolores tempore neque aliquam eligendi repellendus recusandae eveniet dolore ipsa laudantium maxime ad earum dignissimos unde saepe ex quasi fugit? Animi, delectus aliquid maiores corporis tenetur velit dicta provident iusto illum dolores non explicabo nihil facere neque minima commodi magnam tempore nisi ipsam, assumenda in. Ea magni dolorum consequuntur sit similique eum corporis aut reiciendis reprehenderit molestiae, voluptate mollitia? Eos!
+      </p>
+    </div>
+  );
+};
+
+export default About;
