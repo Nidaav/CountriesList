28b3b4d27cbb63080a18b2c365e839f7e6ccd04b
leo tanguy <leo0703@hotmail.fr>
2022-04-08 11:19:08 +0200

    finished just why double bubbles

--- countries-map/src/App.js
+++ countries-map/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+
         {/* Le path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
         <Route path="*" element={<Home />} />
       </Routes>
